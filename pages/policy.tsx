da7e29ec643c9b55c2f3c313105cb5579a34671c
piratepenguin <kihachifuruta@gmail.com>
2025-11-03 16:25:26 +0900

    Update policy.tsx

--- pages/policy.tsx
+++ pages/policy.tsx
@@ -40,7 +40,7 @@ const TextbookPolicyPage: React.FC = () => {
 
         <h3 className={policyStyles.policySectionTitle}>3. 寄付された教科書の所有権について</h3>
         <p className={policyStyles.policyText}>
-          本棚に寄付された教科書の[所有権は放棄]されたものとみなします。一度寄付した本を後から取り戻すことはできません。
+          本棚に<span className={styles.textRed}>寄付された教科書の[所有権は放棄]されたもの</span>とみなします。一度寄付した本を<span className={styles.textRed}>後から取り戻すことはできません。</span>
         </p>
 
         <h3 className={policyStyles.policySectionTitle}>4. 本棚の管理と本の廃棄について</h3>

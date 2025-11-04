243fde75b3a3d1bc7949dafa360fc8deaa2df3df
piratepenguin <kihachifuruta@gmail.com>
2025-11-04 15:45:27 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -223,7 +223,16 @@ const LandingPage = () => {
               <p>エレベーターに向かってすぐ右側のスペースに設置しています。</p>
               <p>開館時間: 平日 8:00〜20:00 / 土曜日 8:00〜12:00　/ 日曜祝日 休館</p>
               <p className={styles.note}>※SAKURAUMの開館時間に準じます。</p>
-               <p> <Link href="https://www.instagram.com/meio_sakuraum_/">最新の会館情報はこちら</Link> </p>
+<p>
+  <Link
+    href="https://www.instagram.com/meio_sakuraum/"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    最新の会館情報はこちら
+  </Link>
+</p>
+
             </div>
          </div>
       </section>
